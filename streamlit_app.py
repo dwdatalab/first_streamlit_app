ed088a47e7b25e5b4a8fda28a220c61eb21725c2
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-03 09:42:22 +0200

    Update streamlit_app.py
    updates single quote error

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,11 @@ import streamlit
 
 🥣 🥗 🐔 🥑🍞
 streamlit.title("My Parents Healthy Diner")
-streamlit.header(" 🥣 🥗 🐔 Breakfast Menu 🥑🍞")
-streamlit.text("Oats 🥣")
-streamlit.text("Kale 🥗 ")
-streamlit.text("Chicken 🐔")
-streamlit.text("Avo and Toast 🥑🍞")
+streamlit.header(' 🥣 🥗 🐔 Breakfast Menu 🥑🍞')
+streamlit.text('Oats 🥣')
+streamlit.text('Kale 🥗 ')
+streamlit.text('Chicken 🐔')
+streamlit.text('Avo and Toast 🥑🍞')
 
-streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
