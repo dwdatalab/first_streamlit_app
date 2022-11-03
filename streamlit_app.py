cf90a522c2b596ef40e49972131ef7b45a8e0ef3
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-03 09:40:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,13 @@
 
 import streamlit
-
+import 
+🥣 🥗 🐔 🥑🍞
 streamlit.title("My Parents Healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("Bacon")
-streamlit.text("Bacon")
-streamlit.text("Bacon")
+streamlit.header(" 🥣 🥗 🐔 Breakfast Menu 🥑🍞")
+streamlit.text("Oats 🥣")
+streamlit.text("Kale 🥗 ")
+streamlit.text("Chicken 🐔")
+streamlit.text("Avo and Toast 🥑🍞")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
