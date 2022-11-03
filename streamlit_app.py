42100e010f564fe2580adcf74f89a5b763d8f797
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-03 09:49:47 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
 import streamlit
 import pandas 
-
+fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 
 streamlit.title("My Parents Healthy Diner")
@@ -12,10 +12,11 @@ streamlit.text('Chicken 🐔')
 streamlit.text('Avo and Toast 🥑🍞')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-# Display the table on the page.
-
-fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(fruit_list)
+
+
+# Display the table on the page.
