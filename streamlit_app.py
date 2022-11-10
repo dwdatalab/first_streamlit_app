01fc6570f983b5230da3fd78d7b2742bcd97b18c
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-10 13:11:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,17 +23,24 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
-# Display the table on the page.
-# Normalize json response
+#New Section to display FruityVice Advice
+streamlit.header('FruitVice Fruit Advice!')
+try: 
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+       streamlit.error("Please select a fruit to get information")
+  else:
+       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice )
+       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+       streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # show response in a dataframe
-streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.stop()
 
