a64e538862ddd76e5d6ce8dccdcfeddce2475f49
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-10 14:00:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,14 +45,14 @@ except URLError as e:
 # show response in a dataframe
 
 
-streamlit.header("The Fruit Load List Contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
          my_cur.execute("SELECT * FROM fruit_load_list")
          return my_cur.fetchall()
         
 # Add a button
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows =  get_fruit_load_list()
     my_cnx.close()
