20d4433294ec78681907b1c5678e393642549282
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-10 13:57:14 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ except URLError as e:
 streamlit.header("The Fruit Load List Contains:")
 def get_fruit_load_list():
     with my_cnx.cursor() AS my_cur:
-         my_cur.execute("SELECT * FROM fruit_load_list ")
+         my_cur.execute("SELECT * FROM fruit_load_list")
          return my_cur.fetchall()
         
 # Add a button
