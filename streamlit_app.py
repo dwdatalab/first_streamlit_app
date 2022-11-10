b431f9b26e91a4a03e2de42d40a6023e68046bee
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-10 12:57:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,5 @@ my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List Contains:")
 streamlit.dataframe(my_data_rows)
 
+add_my_fruit = streamlit.text_input('What fruit would you like to add?','Marula')
+streamlit.write('Thank you for adding', add_my_fruit, 'to the list!')
