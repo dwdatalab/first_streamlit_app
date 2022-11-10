b11953d08260b4881c3370fc6bd80054895eb117
dwdatalab <117283305+dwdatalab@users.noreply.github.com>
2022-11-10 11:50:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,10 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon"
 #streamlit.text(fruityvice_response.json())
 # Display the table on the page.
 # Normalize json response
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # show response in a dataframe
 streamlit.dataframe(fruityvice_normalized)
